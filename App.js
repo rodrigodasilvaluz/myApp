2b559922c4e0561a61a888f64e074fa78024c4da
Rodrigo da Silva Luz <rodrigosl336@gmail.com>
2021-03-05 09:04:28 -0300

    Test with flex and alignment

--- App.js
+++ App.js
@@ -4,10 +4,20 @@ import { View, Text } from 'react-native';
 class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
-        <View style={{ height: 65, backgroundColor: '#ddd' }}></View>
-        <View style={{ flex: 1, backgroundColor: '#fff' }}></View>
-        <View style={{ height: 65, backgroundColor: '#ddd' }}></View>
+      <View style={{
+        // 1, 2, 3, 4 ...
+        flex: 1,
+        // row or column (default)
+        flexDirection: 'column',
+        // center, flex-end, flex-start (default), space-between e space-around
+        justifyContent: 'center',
+        // center, flex-end, flex-start (default), space-between e space-around
+        alignItems: 'center',
+      }}>
+        <View style={{ width: 50, height: 50, backgroundColor: 'green' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'red' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
       </View>
     );
   };
