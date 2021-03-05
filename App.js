2d9d5d4cae8ff52dc338180fab916bc60f20ae76
Rodrigo da Silva Luz <rodrigosl336@gmail.com>
2021-03-05 09:42:24 -0300

    Test with receiving data

--- App.js
+++ App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+
+    this.getName = this.getName.bind(this);
+  }
+
+  getName(text) {
+    this.setState({
+      name: text,
+    });
+  }
+
   render() {
     return (
-      <View style={{
-        // 1, 2, 3, 4 ...
-        flex: 1,
-        // row or column (default)
-        flexDirection: 'column',
-        // center, flex-end, flex-start (default), space-between e space-around
-        justifyContent: 'center',
-        // center, flex-end, flex-start (default), space-between e space-around, stretch
-        alignItems: 'stretch',
-      }}>
-        <View style={{ width: 100, height: 50, backgroundColor: 'green' }}></View>
-        <View style={{ height: 50, backgroundColor: 'red' }}></View>
-        <View style={{ height: 50, backgroundColor: 'yellow' }}></View>
+      <View style={ styles.container }>
+        
+        <TextInput
+          style={ styles.input }
+          placeholder="Typing your name!"
+          underlineColorAndroid="transparent"
+          onChangeText={ this.getName }
+        />
+
+        <Text style={ styles.text }>Wellcome { this.state.name }</Text>
+
       </View>
     );
   };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    margin: 10,
+    fontSize: 20,
+    padding: 10,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25,
+  }
+});
 
 export default App;
