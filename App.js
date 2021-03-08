473890e5ed42e401a2d696ec1fe86bc440fb6cb7
Rodrigo da Silva Luz <rodrigosl336@gmail.com>
2021-03-08 14:50:51 -0300

    Test with FlatList, VERY, VERY IMPORTANT

--- App.js
+++ App.js
@@ -3,76 +3,65 @@ import {
   View,
   Text,
   StyleSheet,
-  ScrollView
+  FlatList
 } from 'react-native';
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        { id: 1, name: 'Rodrigo', age: '45', email: 'rodrigo@rodrigo.com' },
+        { id: 2, name: 'Waleska', age: '42', email: 'waleska@waleska.com' },
+        { id: 3, name: 'Isabel', age: '2', email: 'isabel@isabel.com' },
+        { id: 4, name: 'Augusto', age: '18', email: 'augusto@augusto.com' },
+        { id: 5, name: 'João', age: '10', email: 'joao@joao.com' },
+      ],
+    };
+  }
 
   render() {
     return (
       <View style={ styles.container }>
 
-        <ScrollView scrollEnabled={ true } showsVerticalScrollIndicator={ false }>
-          <View style={ styles.box1 }>
-
-            <ScrollView horizontal={ true } showsHorizontalScrollIndicator={ false }>
-              <View style={ styles.box5 }></View>
-              <View style={ styles.box6 }></View>
-              <View style={ styles.box7 }></View>
-              <View style={ styles.box8 }></View>
-            </ScrollView>
-
-          </View>
-          <View style={ styles.box2 }></View>
-          <View style={ styles.box3 }></View>
-          <View style={ styles.box4 }></View>
-        </ScrollView>
+        <FlatList
+          data={ this.state.feed }
+          keyExtractor={ (item) => item.id }
+          renderItem={ ({item}) => <Pessoa data={ item } /> }
+        />
 
       </View>
     );
   };
 }
 
+class Pessoa extends Component {
+  render() {
+    return (
+      <View style={ styles.areaPeople }>
+        <Text style={ styles.textPeople }>{ this.props.data.name }</Text>
+        <Text style={ styles.textPeople }>{ this.props.data.age }</Text>
+        <Text style={ styles.textPeople }>{ this.props.data.email }</Text>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  box1: {
-    backgroundColor: 'red',
-    height: 250
+  areaPeople: {
+    backgroundColor: '#222',
+    height: 200,
+    marginBottom: 15
   },
-  box2: {
-    backgroundColor: 'green',
-    height: 250
-  },
-  box3: {
-    backgroundColor: 'yellow',
-    height: 250
-  },
-  box4: {
-    backgroundColor: 'blue',
-    height: 250
-  },
-  box5: {
-    backgroundColor: 'red',
-    height: 250,
-    width: 150
-  },
-  box6: {
-    backgroundColor: 'green',
-    height: 250,
-    width: 150
-  },
-  box7: {
-    backgroundColor: 'yellow',
-    height: 250,
-    width: 150
-  },
-  box8: {
-    backgroundColor: 'blue',
-    height: 250,
-    width: 150
-  },
+  textPeople: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
 
 export default App;
